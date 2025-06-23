edc414b8abdb375a8b76b323d267937c337add66
aleks <alekspera10@gmail.com>
2025-06-24 00:26:58 +0300

    Add files via upload

---
+++ FarenhieghttoCelcius/FarenhieghttoCelcius/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace FarenhieghttoCelcius
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCelsuis = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFahrenheit = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCelsuis
+            // 
+            this.txtCelsuis.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCelsuis.Location = new System.Drawing.Point(171, 88);
+            this.txtCelsuis.Name = "txtCelsuis";
+            this.txtCelsuis.Size = new System.Drawing.Size(142, 30);
+            this.txtCelsuis.TabIndex = 0;
+            this.txtCelsuis.TextChanged += new System.EventHandler(this.txtCelsuis_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Celsius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Fahrenheit ";
+            // 
+            // txtFahrenheit
+            // 
+            this.txtFahrenheit.Enabled = false;
+            this.txtFahrenheit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFahrenheit.Location = new System.Drawing.Point(171, 124);
+            this.txtFahrenheit.Name = "txtFahrenheit";
+            this.txtFahrenheit.Size = new System.Drawing.Size(142, 30);
+            this.txtFahrenheit.TabIndex = 3;
+            this.txtFahrenheit.TextChanged += new System.EventHandler(this.txtFahrenheit_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(348, 39);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Celsius to Fahrenheit ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 42);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 230);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFahrenheit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCelsuis);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCelsuis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFahrenheit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FarenhieghttoCelcius/FarenhieghttoCelcius/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FarenhieghttoCelcius
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCelsuis_TextChanged(object sender, EventArgs e)
+        {
+           
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double celsius = 0;
+            double Fahrenheit = 0;
+
+            celsius = double.Parse(txtCelsuis.Text);
+
+            Fahrenheit = (celsius * 9 / 5) + 32;
+
+            txtFahrenheit.Text = Fahrenheit.ToString();
+        }
+
+        private void txtFahrenheit_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
